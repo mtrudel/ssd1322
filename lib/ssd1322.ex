1da19045e4e27c4720202fbe4e30b23035e8aa9b
Mat Trudel <mat@geeky.net>
2020-07-16 20:33:31 -0400

    More docs

--- lib/ssd1322.ex
+++ lib/ssd1322.ex
@@ -5,39 +5,81 @@ defmodule SSD1322 do
 
   use GenServer
 
+  @doc """
+  Starts a connection to an SSD1322 with the given parameters, wrapped in a GenServer to serialize access to the device.
+  Returns an `{:ok, pid}` tuple where the pid is passed in to the other functions in this module.
+
+  Options are passed as a keyword list with the following possible values:
+
+  * `spi_connection_opts`: A nested keyword list containing any of the possible 
+  values below:
+      * `spi_dev`: The name of the spi device to connect to. Defaults to `spidev0.0`
+      * `dc_pin`: The GPIO pin number of the line to use for D/C select. Defaults to 24
+      * `reset_pin`: The GPIO pin number of the line to use for reset. Defaults to 25
+  * `conn`: A pre-existing SSD1322.SPIConnection struct, if you already have one
+  * `width`: The width of the display in pixels. Must be a multiple of 4. Defaults to 256
+  * `height`: The height of the display in pixels. Defaults to 64
+  """
   def start_link(args \\ []) do
     name = args |> Keyword.get(:name)
     GenServer.start_link(__MODULE__, args, name: name)
   end
 
+  @doc """
+  Issues a reset to the SSD1322 device.
+  """
   def reset(pid) do
     GenServer.call(pid, :reset)
   end
 
+  @doc """
+  Turns the display on.
+  """
   def display_on(pid) do
     GenServer.call(pid, :display_on)
   end
 
+  @doc """
+  Turns the display off.
+  """
   def display_off(pid) do
     GenServer.call(pid, :display_off)
   end
 
+  @doc """
+  Sets the contrast of the display. Valid values range from 0 (lowest contrast) to 255 (highest contrast).
+  """
   def contrast(pid, contrast \\ 0xFF) do
     GenServer.call(pid, {:contrast, contrast})
   end
 
-  def clear(pid, grey \\ 0x00) do
+  @doc """
+  Clears the display to the specified grey level. 
+  Valid values for `grey` are from 0 (black) to 15 (whatever colour your display is). Defaults to 0.
+  """
+  def clear(pid, grey \\ 0) do
     GenServer.call(pid, {:clear, grey})
   end
 
+  @doc """
+  Draws the specified bitmap. The bitmap must be in packed 4-bit greyscale format and the size
+  of the full display as configured. For pixel format details see SSD1322.Device.draw.
+  """
   def draw(pid, bitmap) do
     GenServer.call(pid, {:draw, bitmap})
   end
 
+  @doc """
+  Draws the specified bitmap at coordinates `{x, y}`. The bitmap must be in packed 4-bit greyscale format and the size
+  corresponding to the specified width & height. For pixel format details see SSD1322.Device.draw.
+
+  Both `x` and `width` must be a multiple of 4.
+  """
   def draw(pid, bitmap, {x, y}, {w, h}) do
     GenServer.call(pid, {:draw, bitmap, {x, y}, {w, h}})
   end
 
+  @doc false
   def init(args) do
     {:ok, SSD1322.Device.init(args)}
   end
--- lib/ssd1322/device.ex
+++ lib/ssd1322/device.ex
@@ -11,10 +11,25 @@ defmodule SSD1322.Device do
 
   defstruct conn: nil, width: 0, height: 0
 
-  @gdram_row_width 480
-
   alias SSD1322.SPIConnection
 
+  @gdram_row_width 480
+
+  @doc """
+  Sets up a connection to an SSD1322. Returns an opaque struct of type 
+  SSD1322.Device suitable for passing to other functions in this module.
+
+  Options are passed as a keyword list with the following possible values:
+
+  * `spi_connection_opts`: A nested keyword list containing any of the possible 
+  values below:
+      * `spi_dev`: The name of the spi device to connect to. Defaults to `spidev0.0`
+      * `dc_pin`: The GPIO pin number of the line to use for D/C select. Defaults to 24
+      * `reset_pin`: The GPIO pin number of the line to use for reset. Defaults to 25
+  * `conn`: A pre-existing SSD1322.SPIConnection struct, if you already have one
+  * `width`: The width of the display in pixels. Must be a multiple of 4. Defaults to 256
+  * `height`: The height of the display in pixels. Defaults to 64
+  """
   def init(opts \\ []) do
     spi_connection_opts = opts |> Keyword.get(:spi_connection_opts, [])
 
@@ -31,6 +46,9 @@ defmodule SSD1322.Device do
     session
   end
 
+  @doc """
+  Issues a reset to the SSD1322 device.
+  """
   def reset(%__MODULE__{conn: conn}) do
     SPIConnection.reset(conn)
     SPIConnection.command(conn, <<0xFD>>, <<0x12>>)
@@ -53,24 +71,48 @@ defmodule SSD1322.Device do
     SPIConnection.command(conn, <<0xA9>>)
   end
 
+  @doc """
+  Turns the display on.
+  """
   def display_on(%__MODULE__{conn: conn}) do
     SPIConnection.command(conn, <<0xAF>>)
   end
 
+  @doc """
+  Turns the display off.
+  """
   def display_off(%__MODULE__{conn: conn}) do
     SPIConnection.command(conn, <<0xAE>>)
   end
 
+  @doc """
+  Sets the contrast of the display. Valid values range from 0 (lowest contrast) to 255 (highest contrast).
+  """
   def contrast(%__MODULE__{conn: conn}, contrast) do
     SPIConnection.command(conn, <<0xC1>>, <<contrast::8>>)
   end
 
+  @doc """
+  Clears the display to the specified grey level. 
+  Valid values for `grey` are from 0 (black) to 15 (whatever colour your display is). Defaults to 0.
+  """
   def clear(%__MODULE__{width: width, height: height} = device, grey \\ 0) do
     draw(device, :binary.copy(<<grey::4, grey::4>>, div(width * height, 2)), {0, 0}, {width, height})
   end
 
-  # This function does not perform any clipping or validation on the given binary other than to  other than to 
-  # validate that its x offset and width are both multiples of 4
+  @doc """
+  Draws the specified bitmap. The bitmap must be in packed 4-bit greyscale format and the size
+  of the full display as configured.
+
+  The pixel format is packed 4-bit greyscale in the following format
+  * Row length must be a multiple of 4
+  * Each pixel is represented by 4 bits from 0 (fully off) to 15 (fully on)
+  * Pixels are packed 2 per byte, in left-to-right order
+  * Rows are packed in top-to-bottom order
+
+  This function does not perform any clipping or validation on the given binary other than to  other than to 
+  validate that its x offset and width are both multiples of 4.
+  """
   def draw(%__MODULE__{conn: conn, width: display_width}, binary, {x, y}, {width, height}) do
     if rem(x, 4) != 0, do: raise("Cannot draw when x is not divisible by 4 (x=#{x})")
     if rem(width, 4) != 0, do: raise("Cannot draw when width is not divisible by 4 (width=#{width})")
