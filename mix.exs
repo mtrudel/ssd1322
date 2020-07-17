98e1f272f277d5b51ea786b2a44620a9c3ae9162
Mat Trudel <mat@geeky.net>
2020-07-16 20:33:50 -0400

    Version bump to 0.3.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SSD1322.MixProject do
   def project do
     [
       app: :ssd1322,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
